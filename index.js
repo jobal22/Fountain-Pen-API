e4a1682c2f33bf2a314427fcfdb982f34c7438d5
Jobal <jbaldwije@gmail.com>
2020-02-25 20:09:55 -0600

    working on etsy

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const ebayApiKey = '';
-const searchEbayURL = '';
+const etsyApiKey = 'zlz871aunsfnzwupy482id1z';
+const searchEtsyURL = 'https://openapi.etsy.com/v2/listings/active';
 
 const googleApiKey = 'AIzaSyAop8x0uttiuQ-azRPZbR1K07SdFRFZnyg';
-const searchGoogleURL = '';
+const searchGoogleURL = 'https://www.googleapis.com/customsearch/v1';
 
 const youtubeApiKey = 'AIzaSyBmKz0IYHWnKb5ztZXPEIYHGLlzxQ9ff-0';
 const searchYoutubeURL = 'https://www.googleapis.com/youtube/v3/search';
@@ -16,8 +16,32 @@ function formatQueryParams(params) {
     return queryItems.join('&');
 }
 
-function displayResults(responseJson) {
-    // if there are previous results, remove them
+function getEtsyPens(query, maxResults) {
+    const params = {
+        key: etsyApiKey,
+        title: query,
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchEtsyURL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      })
+}
+
+//youtube works!!
+
+/*function displayResults(responseJson) {
     console.log(responseJson);
     $('#youtubeResults-list').empty();
     for (let i = 0; i < responseJson.items.length; i++){
@@ -31,12 +55,12 @@ function displayResults(responseJson) {
     //$('#youtubeResults-list').removeClass('hidden');
   };
 
-function getYoutubePens(query, /*maxResults*/) {
+function getYoutubePens(query, maxResults) {
     const params = {
         key: youtubeApiKey,
         q: query + '%fountain' + '%pen' + '%review',
         part: 'snippet',
-        /*maxResults,*/
+        maxResults,
         type: 'video'
     };
     const queryString = formatQueryParams(params)
@@ -55,15 +79,42 @@ function getYoutubePens(query, /*maxResults*/) {
       .catch(err => {
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
       })
-}
+}*/
 
 function watchForm () {
     $('form').submit(event => {
         event.preventDefault();
         const searchTerm = $('#js-search-term').val();
         const maxResults = 4;
-        getYoutubePens(searchTerm, maxResults);
+        getEtsyPens(searchTerm, maxResults);
+        //getYoutubePens(searchTerm, maxResults);
+        // didnt work!!! getGooglePens(searchTerm, maxResults);
     });
 }
 
 $(watchForm);
+
+//the google stuff did not work
+/*unction getGooglePens(query, maxResults) {
+    const params = {
+        key: googleApiKey,
+        list: query,
+    };
+
+    const queryString = formatQueryParams(params)
+    const url = searchGoogleURL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      })
+}*/
