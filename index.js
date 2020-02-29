7f4e63db4e58f68402f19a4bf0a6e8e5314329f7
Jobal <jbaldwije@gmail.com>
2020-02-29 17:45:58 -0600

    removed unncessary items

--- index.js
+++ index.js
@@ -3,19 +3,6 @@
 const etsyApiKey = 'zlz871aunsfnzwupy482id1z';
 const searchEtsyURL = 'https://openapi.etsy.com/v2/users/etsystore/listings/active.js';
 
-// const youtubeApiKey = 'AIzaSyBmKz0IYHWnKb5ztZXPEIYHGLlzxQ9ff-0';
-// const searchYoutubeURL = 'https://www.googleapis.com/youtube/v3/search';
- 
-//this is for a hamburger menu button
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
 //controls jumper button when clicked
   $(".jumper").on("click", function( e ) {
     
@@ -26,61 +13,3 @@ const searchEtsyURL = 'https://openapi.etsy.com/v2/users/etsystore/listings/acti
     }, 600);
     
 });
-
-// youtube api
-// function formatQueryParams(params) {
-//     const queryItems = Object.keys(params)
-//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//     return queryItems.join('&');
-// }
-
-// function displayResults(responseJson) {
-//     console.log(responseJson);
-//     $('#youtubeResults-list').empty();
-//     for (let i = 0; i < responseJson.items.length; i++){
-//       $('#youtubeResults-list').append(
-//         `<li><h3>${responseJson.items[i].snippet.title}</h3>
-//         <p>${responseJson.items[i].snippet.description}</p>
-//         <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-//         </li>`
-//       )};
-//     //display the results section  
-//     //$('#youtubeResults-list').removeClass('hidden');
-//   };
-
-// function getYoutubePens(query, maxResults) {
-//     const params = {
-//         key: youtubeApiKey,
-//         q: query + '%fountain' + '%pen' + '%review',
-//         part: 'snippet',
-//         maxResults,
-//         type: 'video'
-//     };
-//     const queryString = formatQueryParams(params)
-//     const url = searchYoutubeURL + '?' + queryString;
-
-//     console.log(url);
-
-//     fetch(url)
-//     .then(response => {
-//       if(response.ok) {
-//         return response.json();
-//       }
-//       throw new Error(response.statusText);
-//     })
-//     .then(responseJson => displayResults(responseJson))
-//       .catch(err => {
-//         $('#js-error-message').text(`Something went wrong: ${err.message}`);
-//       })
-// }
-
-// function watchForm () {
-//     $('form').submit(event => {
-//         event.preventDefault();
-//         const searchTerm = $('#js-search-term').val();
-//         const maxResults = 4;
-//         getYoutubePens(searchTerm, maxResults);
-//     });
-// }
-
-// $(watchForm);
